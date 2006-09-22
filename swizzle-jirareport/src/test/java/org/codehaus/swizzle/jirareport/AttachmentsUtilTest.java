d2ef373a5f33b7c33c58cf3f54cc2dfa481d1cda
dblevins <dblevins@ad359b83-b104-0410-b8c9-b39b089e6ab5>
2006-09-22 01:38:03 +0000

    Util for saving attachments

---
+++ swizzle-jirareport/src/test/java/org/codehaus/swizzle/jirareport/AttachmentsUtilTest.java
@@ -0,0 +1,55 @@
+/**
+ *
+ * Copyright 2006 David Blevins
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.codehaus.swizzle.jirareport;
+
+import junit.framework.TestCase;
+import org.codehaus.swizzle.jira.Jira;
+import org.codehaus.swizzle.jira.Issue;
+import org.codehaus.swizzle.jira.Attachment;
+
+import java.util.List;
+import java.io.File;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class AttachmentsUtilTest extends TestCase {
+    public void testSaveAttachment() throws Exception {
+        AttachmentsUtil attachmentsUtil = new AttachmentsUtil();
+
+        Jira jira = new Jira("http://jira.codehaus.org/rpc/xmlrpc");
+        jira.login("swizzletester", "swizzle");
+        jira.autofill("attachments", true);
+
+        Issue issue = jira.getIssue("SWIZZLE-13");
+        assertNotNull("issue", issue);
+        assertEquals("Issue.getKey()", "SWIZZLE-13", issue.getKey());
+
+        List attachments = issue.getAttachments();
+        assertNotNull("attachments", attachments);
+        assertEquals("attachments.size()", 3, attachments.size());
+
+        Attachment attachment = (Attachment) attachments.get(0);
+        assertEquals("Attachment.getFileName()", "ONE.patch", attachment.getFileName());
+        assertEquals("Attachment.getUrl()", "http://jira.codehaus.org/secure/attachment/22972/ONE.patch", attachment.getUrl().toExternalForm());
+
+        File file = attachmentsUtil.saveAttachment(attachment);
+        assertEquals("File.exists()", true, file.exists());
+        assertEquals("File.getName()", "ONE.patch", file.getName());
+        assertEquals("File.length()", 9, file.length());
+    }
+}

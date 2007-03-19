abde16ed5349c029a309e039802793e0f7f5af49
dblevins <dblevins@ad359b83-b104-0410-b8c9-b39b089e6ab5>
2007-03-19 21:54:25 +0000

    Added caching

--- swizzle-jiramacro/src/main/java/org/codehaus/swizzle/jiramacro/SwizzleJiraMacro.java
+++ swizzle-jiramacro/src/main/java/org/codehaus/swizzle/jiramacro/SwizzleJiraMacro.java
@@ -16,6 +16,8 @@
  */
 package org.codehaus.swizzle.jiramacro;
 
+import com.atlassian.confluence.core.ContentEntityObject;
+import com.atlassian.confluence.renderer.PageContext;
 import com.atlassian.confluence.renderer.radeox.macros.MacroUtils;
 import com.atlassian.renderer.v2.RenderMode;
 import com.atlassian.renderer.v2.SubRenderer;
@@ -31,11 +33,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Map;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Iterator;
-import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 
 /**
@@ -43,32 +48,128 @@ import java.lang.reflect.InvocationTargetException;
  */
 public class SwizzleJiraMacro extends AbstractPanelMacro {
 
-    protected String getPanelContentCSSClass() {
-        return "jirareportContent";
+    private WeakHashMap cache = new WeakHashMap();
+    private long timeToLive = (60 * 60 * 1000);  // 1 hour
+
+    public static class Key {
+        private static MessageDigest md;
+
+        static {
+            try {
+                md = MessageDigest.getInstance("SHA");
+            } catch (NoSuchAlgorithmException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+
+        private final byte[] digest;
+        private final int hash;
+
+        public Key(String body) {
+            hash = body.hashCode();
+            digest = md.digest(body.getBytes());
+        }
+
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            final Key key = (Key) o;
+
+            return MessageDigest.isEqual(digest, key.digest);
+        }
+
+        public int hashCode() {
+            return hash;
+        }
     }
 
-    protected String getPanelCSSClass() {
-        return "jirareport";
+    public static class Entry {
+        private final String content;
+        private final long created;
+
+        public Entry(String content) {
+            this.content = content;
+            this.created = System.currentTimeMillis();
+        }
+
+        public boolean isTimedOut(long timeOut) {
+            return created + timeOut < System.currentTimeMillis();
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public long getCreated() {
+            return created;
+        }
     }
 
-    protected String getPanelHeaderCSSClass() {
-        return "jirareportHeader";
+    public static class PageCache {
+        private final String name;
+        private final int version;
+        private final WeakHashMap cache = new WeakHashMap();
+
+        public PageCache(String name, int version) {
+            this.name = name;
+            this.version = version;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getVersion() {
+            return version;
+        }
+
+        public WeakHashMap getCache() {
+            return cache;
+        }
     }
 
-    public RenderMode getBodyRenderMode() {
-        return RenderMode.allow(RenderMode.F_NONE);
-    }
-
-    public boolean suppressMacroRenderingDuringWysiwyg() {
-        return false;
-    }
-
-    protected SubRenderer getSubRenderer() {
-        return super.getSubRenderer();
+    private synchronized WeakHashMap getCache(ContentEntityObject page) {
+        String name = page.getNameForComparison();
+        PageCache pageCache = (PageCache) cache.get(name);
+        if (pageCache == null) {
+            pageCache = new PageCache(name, page.getVersion());
+            cache.put(name, pageCache);
+        } else if (pageCache.getVersion() < page.getVersion()) {
+            pageCache.getCache().clear();
+            pageCache = new PageCache(name, page.getVersion());
+            cache.put(name, pageCache);
+        }
+        return pageCache.getCache();
     }
 
     public String execute(Map params, String body, com.atlassian.renderer.RenderContext renderContext) throws MacroException {
+        PageContext pageContext = (PageContext) renderContext;
+        ContentEntityObject entity = pageContext.getEntity();
 
+        // One cache per page, one synchronized block to get it
+        // this would affect read/writes on all pages
+        WeakHashMap cache = getCache(entity);
+
+        // Now that we have the cache for just this page,
+        // synchronized on it and do our work.
+        synchronized (cache) {
+            Key key = new Key(body);
+            Entry entry = (Entry) cache.get(key);
+
+            if (entry != null && !entry.isTimedOut(timeToLive)) {
+                return entry.getContent();
+            }
+
+            String content = generateContent(params, body, renderContext);
+
+            cache.put(key, new Entry(content));
+
+            return content;
+        }
+    }
+
+    private String generateContent(Map params, String body, com.atlassian.renderer.RenderContext renderContext) throws MacroException {
         URL templateUrl = null;
         try {
             String url = (String) params.get("template");
@@ -115,7 +216,7 @@ public class SwizzleJiraMacro extends AbstractPanelMacro {
         } catch (Throwable e) {
             if (e instanceof InvocationTargetException) {
                 InvocationTargetException ite = (InvocationTargetException) e;
-                e = ite.getCause(); 
+                e = ite.getCause();
             }
 
             CharArrayWriter trace = new CharArrayWriter();
@@ -145,4 +246,29 @@ public class SwizzleJiraMacro extends AbstractPanelMacro {
     public String getName() {
         return "swizzlejira";
     }
+
+    protected String getPanelContentCSSClass() {
+        return "jirareportContent";
+    }
+
+    protected String getPanelCSSClass() {
+        return "jirareport";
+    }
+
+    protected String getPanelHeaderCSSClass() {
+        return "jirareportHeader";
+    }
+
+    public RenderMode getBodyRenderMode() {
+        return RenderMode.allow(RenderMode.F_NONE);
+    }
+
+    public boolean suppressMacroRenderingDuringWysiwyg() {
+        return false;
+    }
+
+    protected SubRenderer getSubRenderer() {
+        return super.getSubRenderer();
+    }
+
 }

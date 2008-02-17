ab8dfea578f0c1aecc7d52bf9b1f45b0c322d25d
dblevins <dblevins@ad359b83-b104-0410-b8c9-b39b089e6ab5>
2008-02-17 22:40:19 +0000

    Can now handle converted or non-converted map formats.   - Data will lazily convert itself on first access.   - toMap forces strings, toRawMap lets the values go as is Slightly better date reading. Date writing still limited.

--- swizzle-confluence/src/main/java/org/codehaus/swizzle/confluence/MapObject.java
+++ swizzle-confluence/src/main/java/org/codehaus/swizzle/confluence/MapObject.java
@@ -20,53 +20,62 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @version $Revision$ $Date$
  */
 public class MapObject {
 
-    private final SimpleDateFormat format;
+    private static final SimpleDateFormat[] formats;
+    static {
+        formats = new SimpleDateFormat[]{
+                new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy"),
+                new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z"),
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S"),
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SZ"),
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S"),
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ"),
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"),
+                new SimpleDateFormat("yyyy-MM-dd HH:mmZ"),
+                new SimpleDateFormat("yyyy-MM-dd HH:mm"),
+                // XML-RPC spec compliant iso8601 formats
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SZ"),
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S"),
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ"),
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"),
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ"),
+                new SimpleDateFormat("yyyy-MM-dd'T'HH:mm"),
+                new SimpleDateFormat("yyyy-MM-dd"),
+                new SimpleDateFormat("yyyyMMdd"),
+        };
+    }
+    
     private final Map fields;
 
     protected MapObject() {
-        fields = new HashMap();
-        format = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy");
+        this(new HashMap());
     }
 
     protected MapObject(Map data) {
         fields = new HashMap(data);
-        format = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy");
-
-        for (Iterator iterator = fields.entrySet().iterator(); iterator.hasNext();) {
-            Map.Entry entry = (Map.Entry) iterator.next();
-            String key = (String) entry.getKey();
-            Object o = entry.getValue();
-
-            if (o instanceof String) continue;
-
-            if (o instanceof Integer) setInt(key, ((Integer) o).intValue());
-            else if (o instanceof Boolean) setBoolean(key, ((Boolean) o).booleanValue());
-            else if (o instanceof Date) setDate(key, ((Date) o));
-            else throw new IllegalStateException("Unsupported data type: "+o.getClass().getName());
-
-        }
     }
 
     protected String getString(String key) {
-        return (String) fields.get(key);
-    }
+        Object o = fields.get(key);
 
-    protected boolean getBoolean(String key) {
-        String value = getString(key);
-        return (value.equalsIgnoreCase("true") || value.equals("1") || value.equalsIgnoreCase("yes"));
-    }
+        if (o instanceof String) {
+            return (String) o;
+        }
 
-    protected int getInt(String key) {
-        String value = getString(key);
-        return Integer.parseInt(value);
+        if (o == null) return null;
+
+        if (o instanceof Date) {
+            return fromDate((Date) o);
+        }
+
+        return o.toString();
     }
 
     protected void setString(String key, String value) {
@@ -74,32 +83,113 @@ public class MapObject {
     }
 
     protected void setInt(String key, int value) {
-        fields.put(key, Integer.toString(value));
+        fields.put(key, new Integer(value));
+    }
+
+    protected void setInt(String key, String value) {
+        fields.put(key, new Integer(value));
+    }
+
+    protected int getInt(String key) {
+        Object o = fields.get(key);
+
+        if (o instanceof Integer) {
+            return ((Integer) o).intValue();
+        }
+
+        if (o instanceof String) {
+            setInt(key, (String) o);
+            return getInt(key);
+        }
+
+        if (o == null) return 0;
+
+        throw new IllegalStateException("Field '" + key + "' is of unknown type: " + o.getClass().getName());
     }
 
     protected void setBoolean(String key, boolean value) {
-        fields.put(key, Boolean.toString(value));
+        fields.put(key, new Boolean(value));
     }
 
+    protected void setBoolean(String key, String value) {
+        boolean b = (value.equalsIgnoreCase("true") || value.equals("1") || value.equalsIgnoreCase("yes"));
+        setBoolean(key, b);
+    }
+
+    protected boolean getBoolean(String key) {
+        Object o = fields.get(key);
+
+        if (o instanceof Boolean) {
+            return ((Boolean) o).booleanValue();
+        }
+
+        if (o instanceof String) {
+            setBoolean(key, (String) o);
+            return getBoolean(key);
+        }
+
+        if (o == null) return false;
+
+        throw new IllegalStateException("Field '" + key + "' is of unknown type: " + o.getClass().getName());
+    }
+
+
     protected void setDate(String key, Date value) {
-        fields.put(key, format.format(value));
+        fields.put(key, value);
+    }
+
+    protected void setDate(String key, String value) {
+        fields.put(key, toDate(value));
     }
 
     protected Date getDate(String key) {
-        try {
-            return format.parse(getString(key));
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return new Date();
+        Object o = fields.get(key);
+
+        if (o instanceof Date) {
+            return (Date) o;
         }
+
+        if (o instanceof String) {
+            setDate(key, (String) o);
+            return getDate(key);
+        }
+
+        if (o == null) return null;
+
+        throw new IllegalStateException("Field '" + key + "' is of unknown type: " + o.getClass().getName());
+    }
+
+    private String fromDate(Date value) {
+        return formats[0].format(value);
+    }
+
+    private Date toDate(String value) {
+        if (value == null || value.equals("")) return new Date();
+
+        ParseException notParsable = null;
+        for (int i = 0; i < formats.length; i++) {
+            try {
+                return formats[i].parse(value);
+            } catch (ParseException e) {
+                notParsable = e;
+            }
+        }
+
+        notParsable.printStackTrace();
+        return new Date();
     }
 
     public Map toMap() {
-        return new HashMap(fields);
+        HashMap map = new HashMap(fields.size());
+        for (Iterator i = fields.keySet().iterator(); i.hasNext();) {
+            String key = (String) i.next();
+            map.put(key, getString(key));
+        }
+        return map;
     }
 
-    public Map toRawMap(){
-        return toMap();
+    public Map toRawMap() {
+        return new HashMap(fields);
     }
 
     public String toString() {
